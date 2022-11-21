669bf544ec8dc6d447a2fdfab417506fc306193b
josemiidev <josemiguel.lorenzolara1501@gmail.com>
2022-11-21 17:33:26 +0100

    Primer Commit

---
+++ core/src/main/java/com/jmdev/Proyecto.java
@@ -0,0 +1,7 @@
+package com.jmdev;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Proyecto extends ApplicationAdapter {
+}
