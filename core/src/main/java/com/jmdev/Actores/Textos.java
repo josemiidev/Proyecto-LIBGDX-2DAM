0c33075916c647dd38761446af89f39050b57734
josemiidev <josemiguel.lorenzolara1501@gmail.com>
2022-12-02 20:11:55 +0100

    Quitando imports inservibles

--- core/src/main/java/com/jmdev/Actores/Textos.java
+++ core/src/main/java/com/jmdev/Actores/Textos.java
@@ -4,8 +4,6 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Textos extends Actor {
--- core/src/main/java/com/jmdev/JuegoTower.java
+++ core/src/main/java/com/jmdev/JuegoTower.java
@@ -2,7 +2,6 @@ package com.jmdev;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
